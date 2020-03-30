12d607c89698a7f4a6fab6227ec51e3e9c928816
Gabe Mulley <gabe@edx.org>
2020-03-30 07:51:23 -0400

    source from google sheet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
+require("dotenv").config({
+  path: `.env`,
+});
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Pandemic Support`,
+    description: `Support local businesses who are dealing with the repurcussions of covid-19.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -18,14 +29,34 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `pandemic-support`,
+        short_name: `pandemic-support`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: "gatsby-source-google-spreadsheet",
+      options: {
+        spreadsheetId: "1tL6PqwVhj0Aa3COGlmokdjy_QYd6W__HUph1tJRivbI",
+    
+        // The `spreadsheetName` is recommended, but optional
+        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
+        // If you are sourcing multiple sheets, you can set this to distringuish between the source data
+        spreadsheetName: "LocalBusiness",
+    
+        // The `typePrefix` is optional, default value is "GoogleSpreadsheet"
+        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
+        // It can be overridden to fully customize the root query
+        typePrefix: "GoogleSpreadsheet",
+        credentials: {
+          client_email: process.env.GOOGLE_SPREADSHEET_AUTH_EMAIL,
+          private_key: process.env.GOOGLE_SPREADSHEET_AUTH_PRIVATE_KEY
+        }
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
